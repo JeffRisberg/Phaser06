ef8c198f572cebd9b04bfc651b940ef0e3dce373
Jeff Risberg <jrisberg@aol.com>
2015-02-08 23:01:31 -0800

    draw laser beams from tower to targets

--- scripts/extensions/Tower.js
+++ scripts/extensions/Tower.js
@@ -15,12 +15,15 @@ define(['Phaser', 'extensions/Monster'], function (Phaser, Monster) {
         this.range = range;
         this.health = health;
         this.fireRate = fireRate;
-        this.lastShot = game.time.now;
+        this.clearLaserTime = game.time.now;
+        this.nextShotTime = game.time.now;
         this.bulletSpeed = bulletSpeed;
         this.price = price;
         this.bulletSprite = bulletSprite;
 
         this.body.allowGravity = false;
+
+        this.laserBeam = this.game.add.graphics(0, 0);
 
         this.explosion = this.game.add.sprite(this.x, this.y - this.height / 2, 'explosion');
         this.explosion.anchor.x = 0.5;
@@ -34,21 +37,33 @@ define(['Phaser', 'extensions/Monster'], function (Phaser, Monster) {
     Tower.prototype.constructor = Tower;
 
     Tower.prototype.attack = function (tower, monsters) {
-        if (tower.game.time.now > tower.lastShot) {
+        if (tower.game.time.now > tower.clearLaserTime) {
+            tower.laserBeam.clear();
+            tower.clearLaserTime = tower.game.time.now + 10000;
+        }
 
+        if (tower.game.time.now > tower.nextShotTime) {
             var targets = [];
             monsters.forEach(function (monster) {
-                if (Math.abs(tower.game.tilePath[monster.tile].y - tower.yTile) <= tower.range &&
-                    Math.abs(tower.game.tilePath[monster.tile].x - tower.xTile) <= tower.range) {
-                    targets.push(monster);
+                if (monster.alive) {
+                    if (Math.abs(tower.game.tilePath[monster.tile].y - tower.yTile) <= tower.range &&
+                        Math.abs(tower.game.tilePath[monster.tile].x - tower.xTile) <= tower.range) {
+                        targets.push(monster);
+                    }
                 }
             });
             if (targets.length > 0) {
                 tower.explosion.play('explosion', 10, false, false);
+
+                tower.laserBeam.lineStyle(5, 0xFF0000);
+                tower.laserBeam.moveTo(tower.x, tower.y - tower.height / 2);
+                tower.laserBeam.lineTo(targets[0].x, targets[0].y);
+
                 Monster.prototype.damageTaken(targets[0], tower.damage);
                 //Tower.prototype.fire(tower, targets[0]);
             }
-            tower.lastShot = tower.game.time.now + tower.fireRate;
+            tower.clearLaserTime = tower.game.time.now + 40;
+            tower.nextShotTime = tower.game.time.now + tower.fireRate;
         }
     };
 
@@ -67,6 +82,10 @@ define(['Phaser', 'extensions/Monster'], function (Phaser, Monster) {
 
     Tower.prototype.death = function (tower) {
         console.log('tower destroyed' + tower);
+
+        tower.laserBeam.destroy();
+        tower.explosion.destroy();
+        tower.kill();
     };
 
     return Tower;
